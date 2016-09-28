ae2138fc6dc900fec47b945469c16c86494408c6
montepasa <ibrahimsamedaker@gmail.com>
2016-09-28 14:04:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+   console.log("app içinde");
+   document.write("app baglantı sağlandı");
 });
 
 io.on('connection', function(socket){
+  console.log("baglantı");
+  document.write("baglantı sağlandı");
     io.emit('ibrahim');
 });
 
