5f9502aec52f504c96b48aaaac179ee631328be5
montepasa <ibrahimsamedaker@gmail.com>
2016-09-28 13:55:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
-var port = process.env.PORT || 3000;
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io').listen(process.env.PORT||3000);
+var io = require('socket.io').listen(process.env.PORT || 1337;);
 
 app.get('/', function(req, res){
  res.writeHead(200, { 'Content-Type': 'text/plain' });
