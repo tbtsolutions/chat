8867c4a8a774dfc3b1257ab7900a84adf6851be4
montepasa <ibrahimsamedaker@gmail.com>
2016-09-28 13:08:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+
+var port = process.env.PORT || 1337;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+ res.writeHead(200, { 'Content-Type': 'text/plain' });
+ res.end('Hello World\n');
 });
 
 io.on('connection', function(socket){
   io.emit("ibrahim");
 });
 
-http.listen(3000, function(){
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
